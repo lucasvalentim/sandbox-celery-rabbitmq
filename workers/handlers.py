d02ca5cf77f249fce62ae2d7cf28cc2560490ef5
Arthur Alvim <afmalvim@gmail.com>
2018-12-17 11:58:59 -0300

    Utilizando variaveis de ambiente.

--- workers/handlers.py
+++ workers/handlers.py
@@ -1,13 +1,11 @@
-import asyncio
 from raven import Client
 from loafer.ext.sentry import sentry_handler
 from utils import sum_
+from decouple import config
+
 
 async def handler_add(data, *args):
-    # await asyncio.sleep(0.1)
-
     return sum_(data['x'], data['y'])
 
-client = Client('https://83593e0e1ca54697a7631c1ced7adca1:d4ed7284a9244625b85eede5a7f4053b@sentry.io/1353744')
-
+client = Client(config('SENTRY_TOKEN_URL'))
 error_handler = sentry_handler(client, delete_message=False)
--- workers/routes.py
+++ workers/routes.py
@@ -1,8 +1,12 @@
 from loafer.ext.aws.routes import SQSRoute
-from .handlers import handler_add, error_handler
+from .handlers import handler_add
+from .handlers import error_handler
+from decouple import config
+
 
 routes = (
-    SQSRoute('loafer-test', {'options': {'WaitTimeSeconds': 3}},
+    SQSRoute(config('QUEUE_NAME', 'loafer-test'),
+             {'options': {'WaitTimeSeconds': 3}},
              handler=handler_add,
              error_handler=error_handler),
 )
